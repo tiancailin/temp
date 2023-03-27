7495d9b66a500db797c1650ac7147883b9ad1f32
tiancailin <linweiming@hotmail.com>
2023-03-28 02:05:22 +0800

    flask error 02

--- run.py
+++ run.py
@@ -1,37 +1,41 @@
 from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
-import re
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    # 要爬取的页面内容
     url = 'https://www.douyu.com/g_dance'
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
-    # input()
-    
-    # 获取CSS、JS等静态资源链接
+    # 伪装成浏览器发送请求
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
     css_links = []
     js_links = []
+
+    # 获取CSS和JS的链接
     for link in soup.find_all('link'):
-        if link.get('rel') == ['stylesheet']:
+        if link.get('href') and '.css' in link.get('href'):
             css_links.append(link.get('href'))
-    for script in soup.find_all('script'):
-        src = script.get('src')
-        if src and re.match(r'.*\.js', src):
-            js_links.append(src)
-    
-    # 拼接CSS、JS链接为绝对路径
-    base_url = 'https://www.douyu.com'
-    css_links = [base_url + link if not link.startswith('http') else link for link in css_links]
-    js_links = [base_url + link if not link.startswith('http') else link for link in js_links]
-    
-    return render_template('index.html', content=soup.find('div', class_='layout-Module-container layout-Cover ListContent'), css_links=css_links, js_links=js_links)
+    for link in soup.find_all('script'):
+        if link.get('src') and '.js' in link.get('src'):
+            js_links.append(link.get('src'))
+
+    # 获取CSS和JS的内容
+    css_content = []
+    js_content = []
+    for link in css_links:
+        css_content.append(requests.get(link).text)
+    for link in js_links:
+        js_content.append(requests.get(link).text)
+
+    # 获取页面内容
+    html_content = str(soup.find('div', {'id': 'js-list-body'}).decode_contents())
+
+    return render_template('index.html', css=css_content, js=js_content, html=html_content)
 
 if __name__ == '__main__':
     app.run(port=8888,debug=True)
